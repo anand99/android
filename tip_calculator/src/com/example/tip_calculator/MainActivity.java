6d739b89f22a73850962bad89c289ab0beca5f60
Anand <anand.modh@yahoo.com>
2013-05-31 10:35:20 -0700

    adding comments to assignment of week1

--- tip_calculator/src/com/example/tip_calculator/MainActivity.java
+++ tip_calculator/src/com/example/tip_calculator/MainActivity.java
@@ -25,23 +25,28 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	//this method called when someone click button
 	public void calculateTip(View view) {
 		
 		EditText totalText=(EditText)findViewById(R.id.totalText);
 		TextView resultView=(TextView)findViewById(R.id.resultText);
 		
+		//get the data from input box in string format
 		String textdata=totalText.getText().toString();
 		
+		//make sure string is non-empty
 		if(textdata.isEmpty()){
 			Context context = getApplicationContext();		
 			CharSequence text = "Please enter total amount.";
 			int duration = Toast.LENGTH_SHORT;
 			Toast toast = Toast.makeText(context, text, duration);
-			toast.show();
+			toast.show();		//Display message to user asking for input
 			resultView.setText("");
 			return;
 		}
 		
+		//this is common method, so need to find out which button was clicked.
+		//depending on which button was clicked, we decide factor to multiply with amount.
 		double factor=0;
 		switch(view.getId()){
 		case R.id.btn10:
@@ -57,12 +62,15 @@ public class MainActivity extends Activity {
 			throw new RuntimeException("Unknow button ID");
 		}
 		
+		//now convert that string to double.
 		double total=Double.parseDouble(textdata);
 		double tip = factor * total;
 		
+		//format it to 2 decimal places and put $ in beginning
 		DecimalFormat fmt = new DecimalFormat("0.00");
 		String result="Tip is:  $" + fmt.format(tip);
 		
+		//put result in textview
 		resultView.setText(result);
 	}
 
